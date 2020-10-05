815465c942fde552cdb2200f59d0586c9c3cf932
Cellofun <freckkled.cellofun@gmail.com>
2020-10-05 18:03:27 +0600

    settings fix

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.oldnotebooksrecipes.ru',
