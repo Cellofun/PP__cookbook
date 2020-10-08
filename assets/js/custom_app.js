2324062b40137a8550df8a1383cd1d62ab143a50
Cellofun <freckkled.cellofun@gmail.com>
2020-10-08 15:02:55 +0600

    favicon & tinymce fix

--- assets/js/custom_app.js
+++ assets/js/custom_app.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('body').fadeIn(1);
 
     $('.recipe-description a').attr('target', '_blank');
-});
 
-AOS.init();
+    AOS.init();
+});
--- cookbook/settings.py
+++ cookbook/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
